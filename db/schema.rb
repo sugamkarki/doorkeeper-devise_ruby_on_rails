2e5b3a16ec08cfa679d7accb39d4aa64feeae70e
Sugam Karki <sugamkarki7058@gmail.com>
2022-07-14 11:47:55 +0545

    added role property

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_055857) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_060104) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_055857) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
